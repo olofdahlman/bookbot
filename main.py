e9983562a00ef4ec55541ec3e820d9f5ff757128
olofdahlman <olofdahlman@live.se>
2024-03-16 23:52:06 +0100

    replaced hello world code with code to read and print the book frankenstein; added gitignore file

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        #The print function will print alot of text, the entire book; uncomment it at your own leisure
+        #print(file_contents)
+
+
+main()
