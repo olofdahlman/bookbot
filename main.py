e81e604736e16c9f721d37708490db7955a04d46
olofdahlman <olofdahlman@live.se>
2024-03-17 01:46:40 +0100

    added description to .gitignore, added wordcount function to main

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         #The print function will print alot of text, the entire book; uncomment it at your own leisure
         #print(file_contents)
+        words = file_contents.split()
 
+        word_count = 0
+        for word in words:
+            
+            word_count += 1
+
+        print(word_count)
 
 main()
